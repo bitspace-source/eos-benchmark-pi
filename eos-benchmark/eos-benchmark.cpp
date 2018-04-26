edec1c55f8826d69893b887e9aab6a46099b042f
Zerfix <zerfix@protonmail.com>
2018-04-26 13:48:10 +0000

    removed stray tabs

--- eos-benchmark/eos-benchmark.cpp
+++ eos-benchmark/eos-benchmark.cpp
@@ -7,19 +7,19 @@ class hello : public eosio::contract {
     using contract::contract;
 
     /// @abi action 
-    void pi( uint64_t iterations,uint64_t difficulty, uint64_t precision ) {
+    void pi( uint64_t iterations, uint64_t difficulty, uint64_t precision ) {
         uint64_t offset = 1; 
         for(uint64_t i = 1; i<precision; i++) {
             offset *= 10;
         }
         uint64_t pi = offset;
-	
-	for(uint64_t i = 1; i < iterations*difficulty; i++) {
-	    if(i % 2 == 0)
-		pi += offset/(1+(i*2));
+        
+        for(uint64_t i = 1; i < iterations*difficulty; i++ {
+            if(i % 2 == 0)
+                pi += offset/(1+(i*2));
             else 
-		pi -= offset/(1+(i*2));
-	}
+                pi -= offset/(1+(i*2));
+        }
         pi *= 4;
         
         print("pi: ", pi);
